8875b1232bf25037b270cb7e2162743d13be4db1
Uttam1910 <uttamt1202@gmail.com>
2024-08-07 20:09:18 +0530

    home link at bottom

--- login.php
+++ login.php
@@ -1,37 +1,3 @@
-<?php
-    require('database.php');
-    session_start();
-    if (isset($_SESSION["email"])) {
-        session_destroy();
-    }
-
-    $ref = @$_GET['q'];
-    if (isset($_POST['submit'])) {
-        $email = $_POST['email'];
-        $pass = $_POST['password'];
-        $email = stripslashes($email);
-        $email = addslashes($email);
-        $pass = stripslashes($pass);
-        $pass = addslashes($pass);
-        $email = mysqli_real_escape_string($con, $email);
-        $pass = mysqli_real_escape_string($con, $pass);
-        $str = "SELECT * FROM user WHERE email='$email' and password='$pass'";
-        $result = mysqli_query($con, $str);
-        if ((mysqli_num_rows($result)) != 1) {
-            echo "<center><h3><script>alert('Sorry.. Wrong Username (or) Password');</script></h3></center>";
-            header("refresh:0;url=login.php");
-        } else {
-            $_SESSION['logged'] = $email;
-            $row = mysqli_fetch_array($result);
-            $_SESSION['name'] = $row[1];
-            $_SESSION['id'] = $row[0];
-            $_SESSION['email'] = $row[2];
-            $_SESSION['password'] = $row[3];
-            header('location: welcome.php?q=1');
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -139,7 +105,6 @@
 </head>
 <body>
     <div class="container">
-        <a href="index.php" class="home-link">Home</a>
         <h5>Login to</h5>
         <h4>Quiztie</h4>
         <form method="post" action="login.php" enctype="multipart/form-data">
@@ -157,9 +122,10 @@
                 <button class="btn" name="submit">Login</button>
             </div>
             <div class="form-group text-center">
-                <span class="text-muted">Don't have an account?</span> <a href="register.php" >Register</a> Here..
+                <span class="text-muted">Don't have an account?</span> <a href="register.php" class="text-muted">Register</a> Here..
             </div>
         </form>
+        <a href="index.php" class="home-link">Home</a>
     </div>
 
     <script src="js/jquery.js"></script>
