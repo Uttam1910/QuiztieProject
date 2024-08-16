55f67c5772e3871615796ba70f9374e3e36d642e
Uttam1910 <uttamt1202@gmail.com>
2024-08-16 18:18:36 +0530

    updated login page

--- login.php
+++ login.php
@@ -1,3 +1,37 @@
+<?php
+    require('database.php');
+    session_start();
+    if (isset($_SESSION["email"])) {
+        session_destroy();
+    }
+
+    $ref = @$_GET['q'];
+    if (isset($_POST['submit'])) {
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+        $email = stripslashes($email);
+        $email = addslashes($email);
+        $pass = stripslashes($pass);
+        $pass = addslashes($pass);
+        $email = mysqli_real_escape_string($con, $email);
+        $pass = mysqli_real_escape_string($con, $pass);
+        $str = "SELECT * FROM user WHERE email='$email' and password='$pass'";
+        $result = mysqli_query($con, $str);
+        if ((mysqli_num_rows($result)) != 1) {
+            echo "<center><h3><script>alert('Sorry.. Wrong Username (or) Password');</script></h3></center>";
+            header("refresh:0;url=login.php");
+        } else {
+            $_SESSION['logged'] = $email;
+            $row = mysqli_fetch_array($result);
+            $_SESSION['name'] = $row[1];
+            $_SESSION['id'] = $row[0];
+            $_SESSION['email'] = $row[2];
+            $_SESSION['password'] = $row[3];
+            header('location: welcome.php?q=1');
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +54,7 @@
             align-items: center;
             height: 100vh;
             color: #343a40;
-            text-align: center;   
+            text-align: center;
             overflow: hidden;
         }
         .container {
@@ -90,15 +124,7 @@
         .text-center {
             text-align: center;
         }
-        .home-link {
-            display: inline-block;
-            margin-bottom: 20px;
-            font-size: 16px;
-            color: #007bff;
-            text-decoration: none;
-        }
-        .home-link:hover {
-            color: #0056b3;
+        a.text-muted:hover {
             text-decoration: underline;
         }
     </style>
@@ -122,10 +148,13 @@
                 <button class="btn" name="submit">Login</button>
             </div>
             <div class="form-group text-center">
-                <span class="text-muted">Don't have an account?</span> <a href="register.php" class="text-muted">Register</a> Here..
+                <span class="text-muted">Don't have an account?</span> 
+                <a href="register.php" class="text-muted">Register</a> Here..
+            </div>
+            <div class="form-group text-center">
+                <a href="index.php" class="btn">Go to Home</a>
             </div>
         </form>
-        <a href="index.php" class="home-link">Home</a>
     </div>
 
     <script src="js/jquery.js"></script>
